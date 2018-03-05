f54d2b3269a0146bfde5dc8f71f2a235bb8442ac
Scott Neitzel <scott.neitzel@gmail.com>
2018-03-05 15:07:44 -0600

    .

--- html/togglecook.php
+++ html/togglecook.php
@@ -34,7 +34,7 @@
         }
 		else
 		{
-			exec("sudo ./maverick > /var/www/html/maverick.log &");
+			exec("sudo ./maverick.sh");
 			echo "Stop Cook";
 			sleep(1);
 			if ($result=$database->query($query))
