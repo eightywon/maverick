b129a8bb825fbd849f8757b41d6bae35bf1d063c
Scott Neitzel <scott.neitzel@gmail.com>
2018-03-19 11:24:21 -0500

    0.0.19 added loading animation on graph page

--- html/line2.php
+++ html/line2.php
@@ -1,76 +1,100 @@
 <html>
-   <head>
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script type="text/javascript">
-    google.charts.load('current', {'packages':['corechart','line']});
-	google.charts.setOnLoadCallback(function() {
-		$(function() {
+ <head>
+  <style>
+   #loading-img {
+    background: url(loading.gif) center top no-repeat;
+    height: 100%;
+   }
+
+   .loading {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 20;
+   }
+  </style>
+  <!--Load the AJAX API-->
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script type="text/javascript">
+   google.charts.load('current', {'packages':['corechart','line']});
+   google.charts.setOnLoadCallback(function() {
+	$(function() {
+		//initialize
+		$('.loading').show();
+		callAjax();
+		refreshChart();
+
+		$("#cookid").change(function() {
+			$('#chart_div').html('');
+			$('#toggleLines').hide();
+			$('#selectCook').hide();
+			$('.loading').show();
 			callAjax();
 			refreshChart();
+			if ($("#cookid option:selected").text().indexOf("Active")>=0) {
+				clearInterval(chartInterval);
+				chartInterval=setInterval(refreshChart,10000);
 
-			$("#cookid").change(function() {
-				callAjax();
-				refreshChart();
-				if ($("#cookid option:selected").text().indexOf("Active")>=0) {
-					clearInterval(chartInterval);
-					chartInterval=setInterval(refreshChart,10000);
+				clearInterval(ajaxInterval);
+				ajaxInterval=setInterval(callAjax,10000);
+			} else {
+				clearInterval(chartInterval);
+				clearInterval(ajaxInterval);
+			}
+		});
 
-					clearInterval(ajaxInterval);
-					ajaxInterval=setInterval(callAjax,10000);
-				} else {
-					clearInterval(chartInterval);
-					clearInterval(ajaxInterval);
+		$("#showFood").change(function() {
+			refreshChart();
+		});
+
+		$("#showPit").change(function() {
+			refreshChart();
+		});
+
+		function refreshChart() {
+			$.ajax({
+				url: "getdata.php",
+				data: {'reqType': 'chart', 'cookid': $("#cookid").val()},
+				type: "POST",
+				async: true,
+				dataType: "json",
+				success:function(data) {
+					drawChart(data);
+				}
+			});
+		}
+
+		function callAjax() {
+			$.ajax({
+				url:'getdata.php',
+				type:'POST',
+				data: {'reqType': 'temps', 'cookid': $("#cookid").val()},
+				async: false,
+				dataType: "json",
+				success:function(data){
+					if (data['when'].indexOf("Cook #")>=0) {
+						$("#food").html("");
+						$("#pit").html("");
+					} else {
+						$("#food").html("Food: "+data['probe1']);
+						$("#pit").html("Pit: "+data['probe2']);
+					}
+					$("#when").html(data['when']);
 				}
 			});
 
-			$("#showFood").change(function() {
-				refreshChart();
-			});
+		}
 
-			$("#showPit").change(function() {
-				refreshChart();
-			});
-
-			function refreshChart() {
-				var chartData = $.ajax({
-					url: "getdata.php",
-					data: {'reqType': 'chart', 'cookid': $("#cookid").val()},
-					type: "POST",
-					dataType: "json",
-					async: false
-				}).responseText;
-				drawChart(chartData);
-			}
-
-			//var callAjax = function(){
-			function callAjax() {
-				$.ajax({
-					url:'getdata.php',
-					type:'POST',
-					data: {'reqType': 'temps', 'cookid': $("#cookid").val()},
-					dataType: "json",
-					success:function(data){
-						if (data['when'].indexOf("Cook #")>=0) {
-							$("#food").html("");
-							$("#pit").html("");
-						} else {
-							$("#food").html("Food: "+data['probe1']);
-							$("#pit").html("Pit: "+data['probe2']);
-						}
-						$("#when").html(data['when']);
-					}
-				});
-
-			}
-			if ($("#cookid option:selected").text().indexOf("Active")>=0) {
-				chartInterval=setInterval(refreshChart,10000);
-				ajaxInterval=setInterval(callAjax,10000);
-			}
-
-		});//jquery load
-	}); //google chart
+		//if active cook is being displayed, set callback to refresh chart every 10 seconds
+		if ($("#cookid option:selected").text().indexOf("Active")>=0) {
+			chartInterval=setInterval(refreshChart,10000);
+			ajaxInterval=setInterval(callAjax,10000);
+		}
+	});//jquery load
+   }); //google chart
 
 	var data, chart;
 	function drawChart(chartJson) {
@@ -90,7 +114,7 @@
 						fontSize: 16,
 						fontName: 'Arial',
 						bold: false,
-					italic: true
+						italic: true
 					}
 				},
 				vAxis: {
@@ -117,16 +141,18 @@
 					  ignoreBouds: true,
 					  isHtml: true,
 					  pivot: {x: -50, y: -80}
-					 }
+				}
 			};
-
 			// Create our data table out of JSON data loaded from server.
 			data=new google.visualization.DataTable(chartJson);
+
 			// Instantiate and draw our chart, passing in some options.
 			chart=new google.visualization.LineChart(document.getElementById('chart_div'));
 			chart.draw(data,options);
+
 			//add event for body clicks that clears the tooltip
 			document.body.addEventListener('click',clearSelection,true);
+
 			//add event for tootltip delete data point click to perform the query
 			chart.setAction({
 				id: 'delPoint',
@@ -140,14 +166,20 @@
 						data: {'cookid': $("#cookid").val(), 'time': dtstring},
 						success:function(data){
 							if (data!='fail') {
-								var chartData = $.ajax({
+								$('#chart_div').html('');
+								$('#toggleLines').hide();
+								$('#selectCook').hide();
+								$('.loading').show();
+								$.ajax({
 									url: "getdata.php",
 									data: {'reqType': 'chart', 'cookid': $("#cookid").val()},
 									type: "POST",
 									dataType: "json",
-									async: false
-								}).responseText;
-								drawChart(chartData);
+									async: true,
+									success:function(data) {
+										drawChart(data);
+									}
+								});
 							} else { alert('failed');}
 						}
 					});
@@ -157,7 +189,7 @@
 
 			//show or hide the food or pit graphs based on user input
 			if (!$("#showPit").is(":checked") || !$("#showFood").is(":checked")) {
-				view = new google.visualization.DataView(data);
+				view=new google.visualization.DataView(data);
 				if (!$("#showFood").is(":checked")) {
 					view.hideColumns([1]);
 				}
@@ -167,6 +199,10 @@
 				chart.draw(view, options);
 			}
 
+			//chart loaded, hide loading gif, show UI
+			$('.loading').hide();
+			$('#toggleLines').show();
+			$('#selectCook').show();
 		} else {
 			$("#chart_div").html("");
 		}
@@ -191,17 +227,24 @@
   <body>
    <table width=90% align=center>
     <tr align=center>
-     <td width=50%><h1><div id="pit">Pit: </div></h1></td>
-     <td width=50%><h1><div id="food">Food: </div></h1></td>
+     <td width=50%><h1><div id="pit"></div></h1></td>
+     <td width=50%><h1><div id="food"></div></h1></td>
     </tr>
     <tr align=center>
      <td colspan=2><h2><div id="when"></div></h2></td>
     </tr>
-    <tr align=center><td colspan=2><div id='chart_div'></div></td></tr>
+    <tr align=center>
+     <td colspan=2>
+      <div class="loading"><div id="loading-img"></div></div>
+      <div id='chart_div'></div>
+     </td>
+    </tr>
     <tr align=left>
      <td width=25%>
-      <input type='checkbox' id='showFood' checked>Food</input>&nbsp;
-      <input type='checkbox' id='showPit' checked>Pit</input>
+      <div id="toggleLines" style="display:none">
+       <input type='checkbox' id='showFood' checked>Food</input>&nbsp;
+       <input type='checkbox' id='showPit' checked>Pit</input>
+      </div>
      </td>
      <td>&nbsp;</td>
     </tr>
@@ -217,7 +260,7 @@
 	} else {
 		$activeCook=$database->querySingle('SELECT cookid from activecook;');
 	}
-        echo "    <tr align=left><td width=25%>\n";
+        echo "    <tr align=left><td width=25%><div id='selectCook' style='display:none'>\n";
         echo "     <select id='cookid'>\n";
 	if ($activeCook!='-1') {
 		echo "      <option value='".$activeCook."' selected>Active Cook (#".$activeCook.")</option>";
@@ -233,6 +276,6 @@
 		}
 	}
     ?>
-   </td><td>&nbsp;</td></tr></table>
+   </select></div></td><td>&nbsp;</td></tr></table>
   </body>
 </html>
